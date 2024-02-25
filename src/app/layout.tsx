992f11cd3277413b10af6507860a14ce66c3610e
Abhilash <abhilash301267@gmail.com>
2024-02-25 19:45:20 +0530

    First commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import ScrollToTop from "@/components/ScrollToTop";
+import { Inter } from "next/font/google";
+import "node_modules/react-modal-video/css/modal-video.css";
+import "../styles/index.css";
+import { Providers } from "./providers"; // Ensure this import is at the top with the others
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html suppressHydrationWarning lang="en">
+      <head />
+      <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}>
+        <Providers>
+          {/* Wrap the content that Providers should contain within it */}
+          <Header />
+          {children}
+          {/* <Footer />
+          <ScrollToTop /> */}
+        </Providers>
+      </body>
+    </html>
+  );
+}
